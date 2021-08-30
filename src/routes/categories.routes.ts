04084a5e70871649ebba7c8c959027426451c7af
Guilherme H <54048239+GuilhermeDeveloperBr@users.noreply.github.com>
2021-08-29 21:05:43 -0300

    Add Cad. Category and Specifications

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+
+const CategoriesRoutes = Router();
+
+CategoriesRoutes.post('/', (request, response) => {
+  return createCategoryController.handle(request, response);
+});
+
+CategoriesRoutes.get('/', (request, response) => {
+  return listCategoriesController.handle(request, response);
+});
+
+export { CategoriesRoutes };
